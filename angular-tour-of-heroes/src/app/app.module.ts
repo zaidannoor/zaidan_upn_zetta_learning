94d4b3a64b838e5df2408f5be404d7ef18d35b68
Zaidan Noor <zaidannoor06@gmail.com>
2022-11-02 14:08:37 +0700

    Delete heroes component and style.css

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
   ],
   imports: [
     BrowserModule,
